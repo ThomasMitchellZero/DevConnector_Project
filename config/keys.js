b9286bd960fe333de063d39decc85c58fe9283fe
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2018-11-25 15:25:11 -0500

    Connecting to our mLab Mongo database via a URI stored in an external file via Mongoose.  Also

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+/* I copypasted this sting from my mLab account.  One thing
+to remember is that you have to replace <dbuser> and
+<dbpassword> with your username and password in the string.
+NOT in <> tags! */
+
+mongoURI: 
+"mongodb://ThomasMitchellZero:R3d2na2na@ds115664.mlab.com:15664/wavedesigndatabase"
+}
