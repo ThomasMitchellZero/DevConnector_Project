b616e67122c78284cfd208c412c058bb4468ec9f
ThomasMitchellZero <42942337+ThomasMitchellZero@users.noreply.github.com>
2018-11-26 18:21:54 -0500

    About halfway through S2 L, but I need to get going home.

--- server.js
+++ server.js
@@ -2,31 +2,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+/* links to the new JS files we created for the routing. */
+
+const users = require("./routes/api/users.js");
+const profiles = require("./routes/api/profiles.js");
+const posts = require("./routes/api/posts.js");
+
 const app = express();
 
-//DB configuration
-/* ./config means "find a folder called config in the top-level
-folder."  mongoURI is the key we gave to the value of the URI
-string in our  */
+
 const db = require('./config/keys.js').mongoURI;
 
-//connect to MongoDB
 
-/* Not sure what Mongoose does, but we required it above.
-First, we use the connect method, passing it our URI string
-from the keys.js file as a target.  The bit about the URL
-parser came in response to an error message saying the string
-parser was deprecated.
-
-This mentions Promises, which I don't quite understand yet.  
-but what seems to be happening is that if we successfully
-connect, we console.log an error message and if not, the
-.catch method gets an external error message and then we
-console.log that error.
-
-Making a change to the password or username in the keys.js file
-triggers this change.
- */
 
 mongoose
     .connect(db,{ useNewUrlParser: true })
@@ -35,6 +22,15 @@ mongoose
 
 app.get('/', (req,res) => res.send(`<h1>Small Hello<h1>`));
 
-const port = process.env.PORT || 5000
+/* Not really sure about the syntax here.  Hopefully it will all
+make sense soon. */
+
+app.use("/api/users", users);
+app.use("/api/users", profiles);
+app.use("/api/posts", posts);
+
+
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on ${port}`));
